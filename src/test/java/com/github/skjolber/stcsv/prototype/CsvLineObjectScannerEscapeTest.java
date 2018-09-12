876c57fe2f8199adba2bf8351c2dac7250d5470c
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2018-09-12 17:37:46 +0200

    Make CsvException+IOException the only exceptions thrown from reader per default

--- src/test/java/com/github/skjolber/stcsv/prototype/CsvLineObjectScannerEscapeTest.java
+++ src/test/java/com/github/skjolber/stcsv/prototype/CsvLineObjectScannerEscapeTest.java
@@ -1,6 +1,7 @@
 package com.github.skjolber.stcsv.prototype;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.StringReader;
 
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.github.skjolber.stcsv.CsvReader;
+import com.github.skjolber.stcsv.CsvException;
 import com.github.skjolber.stcsv.CsvMapper;
 
 public class CsvLineObjectScannerEscapeTest {
@@ -113,4 +115,31 @@ public class CsvLineObjectScannerEscapeTest {
 		
 		assertThat(scanner.next()).isNull();
 	}		
+	
+	@Test
+	public void testNoEndQuoteThrowsException() throws Exception {
+		String stringValue = "string \n with \n newline";
+		
+		StringBuffer builder = new StringBuffer();
+		// header
+		builder.append("stringValue");
+		builder.append(",");
+		builder.append("randomValue");
+		builder.append("\n");
+		
+		// first line
+		builder.append('"');
+		builder.append(stringValue);
+		builder.append(",");
+		builder.append("random data");
+		builder.append("\n");
+
+		CsvReader<CsvLineObject> scanner = mapping.create(new StringReader(builder.toString()));
+				
+		assertThrows(CsvException.class, () -> {
+			scanner.next();
+	    });
+
+	}		
+	
 }
